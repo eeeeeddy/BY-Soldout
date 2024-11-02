95b0611ae957f82a11550f856cd203e019b0b852
eeeeeddy <marill94727@naver.com>
2024-11-02 14:22:24 +0900

    수정 배포

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import '../css/Main.css';
+import { PC, Mobile } from "../components/Responsive";
 import Navbar from './navbar';
+import ActionButton from './actionButton';
 
 function Main() {
     return (
@@ -8,39 +10,56 @@ function Main() {
             <div className='Navbar'>
                 <Navbar />
             </div>
-            <div className="scrollspy-example bg-white p-3 rounded-2 mt-5" data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" tabIndex={0}>
 
-                {/* <img src="../../public/images/003.jpg" alt="example" style={{ width: "100%", height: "auto" }} /> */}
-                <div className='image-container'>
-                    <img src={`${process.env.PUBLIC_URL}/images/001.jpg`} alt="img001"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/002.jpg`} alt="img002"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/003.jpg`} alt="img003"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/004.jpg`} alt="img004"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/005.jpg`} alt="img005"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/006.jpg`} alt="img006"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/007.jpg`} alt="img007"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/008.jpg`} alt="img008"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/009.jpg`} alt="img009"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/010.jpg`} alt="img010"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/011.jpg`} alt="img011"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/012.jpg`} alt="img012"/>
-                    <img src={`${process.env.PUBLIC_URL}/images/013.jpg`} alt="img013"/>
+            <PC>
+                <div className="scrollspy-example bg-white p-3 rounded-2 mt-5" data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" tabIndex={0}>
+                    <div className='image-container'>
+                        <div id="sec01" className='mb-4'></div>
+                        <img src={`${process.env.PUBLIC_URL}/images/001.jpg`} alt="img001"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/002.jpg`} alt="img002"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/003.jpg`} alt="img003"/>
+                        <div id="sec02" className='mb-4'></div>
+                        <img src={`${process.env.PUBLIC_URL}/images/004.jpg`} alt="img004"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/005.jpg`} alt="img005"/>
+                        <div id="sec03" className='mb-4'></div>
+                        <img src={`${process.env.PUBLIC_URL}/images/006.jpg`} alt="img006"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/007.jpg`} alt="img007"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/008.jpg`} alt="img008"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/009.jpg`} alt="img009"/>
+                        <div id="sec04" className='mb-4'></div>
+                        <img src={`${process.env.PUBLIC_URL}/images/010.jpg`} alt="img010"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/011.jpg`} alt="img011"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/012.jpg`} alt="img012"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/013.jpg`} alt="img013"/>
+                    </div>
                 </div>
+            </PC>
 
-                
-                
+            <Mobile>
+                <div className="scrollspy-example bg-white p-3 rounded-2 mt-5" data-bs-spy="scroll" data-bs-target="#navbar-example2" data-bs-root-margin="0px 0px -40%" data-bs-smooth-scroll="true" tabIndex={0}>
+                    <div className='image-container'>
+                        <div id="sec01" className='mb-4'></div>
+                        <img src={`${process.env.PUBLIC_URL}/images/001.jpg`} alt="img001"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/002.jpg`} alt="img002"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/003.jpg`} alt="img003"/>
+                        <div id="sec02" className='mb-4'></div>
+                        <img src={`${process.env.PUBLIC_URL}/images/004.jpg`} alt="img004"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/005.jpg`} alt="img005"/>
+                        <div id="sec03" className='mb-4'></div>
+                        <img src={`${process.env.PUBLIC_URL}/images/006.jpg`} alt="img006"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/007.jpg`} alt="img007"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/008.jpg`} alt="img008"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/009.jpg`} alt="img009"/>
+                        <div id="sec04" className='mb-4'></div>
+                        <img src={`${process.env.PUBLIC_URL}/images/010.jpg`} alt="img010"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/011.jpg`} alt="img011"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/012.jpg`} alt="img012"/>
+                        <img src={`${process.env.PUBLIC_URL}/images/013.jpg`} alt="img013"/>
+                    </div>
+                </div>
+            </Mobile>
 
-
-
-                {/* About */}
-                {/* <About /> */}
-
-                {/* Skills */}
-                {/* <Skills /> */}
-                
-                {/* Projects */}
-                {/* <Projects /> */}
-            </div>
+            <ActionButton />
         </div>
     )
 }
--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -1,32 +1,50 @@
 import React from 'react';
+import { PC, Mobile } from "../components/Responsive";
 
 function navbar() {
     return (
         <div>
-            <nav id="navbar-example2" className="navbar bg-dark bg-opacity-25 px-3">
-                <a className="navbar-brand" href="/Portfolio"><strong>분 양 완 판</strong></a>
-                <ul className="nav nav-pills">
-                    <li className='nav-item'>
-                        {/* <a className='nav-link' href='/Portfolio/About'>About</a> */}
-                        <a className='nav-link' href='https://www.naver.com'>Menu 1</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='https://www.naver.com'>Menu 2</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='https://www.naver.com'>Menu 3</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='https://www.naver.com'>Menu 4</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='https://www.naver.com'>Menu 5</a>
-                    </li>
-                    <li className='nav-item'>
-                        <a className='nav-link' href='https://www.naver.com'>Menu 6</a>
-                    </li>
-                </ul>
-            </nav>    
+            <PC>
+                <nav id="navbar-example2" className="navbar bg-dark px-3 fixed-top">
+                    <ul className="nav mx-auto justify-content-center">
+                        <li className='nav-item'>
+                            <a className='nav-link text-white' href='#sec01' style={{ paddingTop: '0px', paddingBottom: '0px'}}>
+                                <img src={`${process.env.PUBLIC_URL}/favicon.png`} alt="logo" style={{ width: '44px', height: 'auto' }}/>
+                            </a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link text-white' href='#sec02'>가격 안내</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link text-white' href='#sec03'>샘플 안내</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link text-white' href='#sec04'>드리는 말</a>
+                        </li>
+                    </ul>
+                </nav>
+            </PC>
+
+            <Mobile>
+                <nav id="navbar-example2" className="navbar bg-dark pe-1 fixed-top">
+                    <ul className="nav mx-auto justify-content-center">
+                        <li className='nav-item'>
+                            <a className='nav-link text-white pe-1' href='#sec01' style={{ paddingTop: '4px', paddingLeft: '8px'}}>
+                                <img src={`${process.env.PUBLIC_URL}/favicon.png`} alt="logo" style={{ width: '34px', height: 'auto' }}/>
+                            </a> 
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link text-white pe-1' href='#sec02'>가격 안내</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link text-white pe-1' href='#sec03'>샘플 안내</a>
+                        </li>
+                        <li className='nav-item'>
+                            <a className='nav-link text-white pe-1' href='#sec04'>드리는 말</a>
+                        </li>
+                    </ul>
+                </nav>
+            </Mobile>
         </div>
     );
 }
